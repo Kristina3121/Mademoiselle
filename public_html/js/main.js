f0455a979d2537e0cbcacb17f0c6e9f0519bcf26
Kristina <kristinadabic26@gmail.com>
2022-12-22 14:06:29 +0100

    project done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,7 @@
 
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     //ANIMATION
     function animation() {
         let  windowHeight = $(window).height();
@@ -25,12 +25,76 @@ $(document).ready(function(){
     animation();
 
     //professional slider
-    
+
     if($('.professional-slider').length > 0){
-         $(".professional-slider").owlCarousel();
+        $(".professional-slider").owlCarousel({
+            
+            loop: true,
+            autoplay: true,
+            autoPlayHoverPause: true,
+            
+            
+            responsive:{
+                0:{
+                    items: 1,
+                    margin:0,
+                    dots: false
+                },
+                992:{
+                    items: 1,
+                    margin: 0
+                }
+            }
+        });
     }
+
+
+
+    //form validator
     
-    
-    
+     if ($('.contact-form').length > 0) {
+
+        $('.contact-form').validate({
+
+            highlight: function (element) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function (element) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    required: true
+
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+
+                message: {
+                    required: true
+                }
+
+            },
+            messages: {
+                name: {
+                    required: 'The Name* field is required'
+                },
+                email: {
+                    required: 'The Email* field is required',
+                    email: 'Please provide a valid email address'
+                },
+                message: {
+                    required: 'The Message* field is required'
+                }
+            },
+
+            errorElement: 'p',
+            errorPlacement: function (error, element) {
+                error.appendTo(element.closest(".form-group").find(".error-msg"));
+            }
+        });
+    }
 });
 
